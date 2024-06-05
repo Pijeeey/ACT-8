4075f25cb61b43cc8e91f02638bed193ebddbbce
Pijeeey <fernandez.pj10@gmail.com>
2024-06-05 23:57:15 +0800

    ACT 8

---
+++ app/src/main/java/com/example/act8/Screen.kt
@@ -0,0 +1,18 @@
+package com.example.act8
+
+import androidx.navigation.NavArgs
+
+sealed class Screen(val route:String)
+{
+    object MainScreen : Screen("main_screen")
+    object DetailScreen : Screen("detail_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
